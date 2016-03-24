5d637540cb9d9a5ce0c569057874354221a7ee19
vikramlance <vikram.lance@gmail.com>
2016-03-23 21:15:49 -0500

    updated find and replace MS word doc script

---
+++ findAndReplaceWordDoc.py
@@ -0,0 +1,62 @@
+import zipfile
+import os
+
+import os.path
+
+
+if (os.path.isfile("C:/projects/Python-Programming/temp.xml")):
+    os.remove("C:/projects/Python-Programming/temp.xml")
+    
+if (os.path.isfile("C:/word/document.xml")):
+    os.remove("C:/word/document.xml")
+
+if (os.path.isfile("C:/projects/Python-Programming/outputFile.docx")):
+    os.remove("C:/projects/Python-Programming/outputFile.docx")
+
+f= open("parameter.txt","r")
+data=f.read()
+
+data_list=data.split("\n")
+newCompany=data_list[0]
+newPosition=data_list[1]
+
+print("Reading parameter file")
+
+print("String to find - Tantalus ")
+
+print("String to replace")
+print(newCompany)
+print("String to find - Sisyphus ")
+
+print("String to replace")
+print(newPosition)
+
+
+
+replaceText = {"Tantalus" : newCompany, "Sisyphus" : newPosition}
+templateDocx = zipfile.ZipFile("C:/projects/Python-Programming/inputFile.docx")
+newDocx = zipfile.ZipFile("C:/projects/Python-Programming/outputFile.docx", "a")
+
+with open(templateDocx.extract("word/document.xml", "C:/")) as tempXmlFile:
+    tempXmlStr = tempXmlFile.read()
+
+for key in replaceText.keys():
+    tempXmlStr = tempXmlStr.replace(str(key), str(replaceText.get(key)))
+
+with open("C:/projects/Python-Programming/temp.xml", "w+") as tempXmlFile:
+    tempXmlFile.write(tempXmlStr)
+
+#print(templateDocx.filelist)
+
+for file in templateDocx.filelist:
+    if not file.filename == "word/document.xml":
+        newDocx.writestr(file.filename, templateDocx.read(file))
+
+newDocx.write("C:/projects/Python-Programming/temp.xml", "word/document.xml")
+
+templateDocx.close()
+newDocx.close()
+
+print("new document with replaced strings is available - C:/projects/Python-Programming/outputFile.docx") 
+
+
