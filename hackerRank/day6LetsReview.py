e4571282f2d14e0ed6ba95cfdcfa2d9b46918fd5
vikramlance <vikram.lance@gmail.com>
2016-04-21 00:06:03 -0500

    new program

---
+++ hackerRank/day6LetsReview.py
@@ -0,0 +1,37 @@
+'''
+Task 
+Given a string, SS, of length N that is indexed from 0 to N−1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+
+Note: 0 is considered to be an even index.
+
+Input Format
+
+The first line contains an integer, T (the number of test cases). 
+Each line ii of the T subsequent lines contain a String, S.
+
+Sample Input
+
+2
+Hacker
+Rank
+Sample Output
+
+Hce akr
+Rn ak
+'''
+
+
+T=int(raw_input())
+
+for i in range (0,T):
+    s=raw_input()
+    p=list(s)
+    e=''
+    o=''
+    for j in range (0,len(p),2):
+    
+        e= e+ p[j]
+    for k in range (1,len(p),2):
+        o= o +p[k] 
+    print "%s %s" %(e,o)
+            
--- hackerRank/test.py
+++ hackerRank/test.py
@@ -1,2 +1,6 @@
-for i in range (0,3):
+for i in range (0,6,2):
     print i
+
+s="mart"
+p=list(s)
+print p
