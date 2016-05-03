1ac631aa60e16bd7b1a74fe2d3827b0830532144
vikramlance <vikram.lance@gmail.com>
2016-05-03 09:28:46 -0500

    utopian tree

--- hackerRank/Algorithms/Implementation/utopian-tree.py
+++ hackerRank/Algorithms/Implementation/utopian-tree.py
@@ -1,3 +1,22 @@
 '''
 
 '''
+
+
+#!/bin/python
+
+import sys
+
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    n = int(raw_input().strip())
+    h=1
+    for i in range (n):
+        if i%2 ==0:
+            h=h+h
+
+        else:
+            h=h+1
+
+    print h
