3d668c8c9c75b4bb728599dca74db5847b6e3c06
vikramlance <vikram.lance@gmail.com>
2016-05-06 23:48:10 -0500

    sql new dir

--- hackerRank/SQL/Basic Select/revising-the-select-query.sql
+++ hackerRank/SQL/Basic Select/revising-the-select-query.sql
@@ -1,1 +1,2 @@
-select * from city where population > 100000 and countrycode ='USA'
+select * from city where population > 100000 and countrycode ='USA';
+select name from city where population > 120000 and countrycode ='USA';
