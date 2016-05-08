0a9b19b8020785b21954e65d200ea37ec489ceab
vikramlance <vikram.lance@gmail.com>
2016-05-08 10:20:13 -0500

    program

--- hackerRank/Algorithms/Implementation/sherlock-and-squares.py
+++ hackerRank/Algorithms/Implementation/sherlock-and-squares.py
@@ -1,12 +1,38 @@
 #!/bin/python
+
+from math import *
+ 
+if __name__ == '__main__':
+    t = input()
+    for a in range(t):
+        n, k = map(int, raw_input().split())
+        n = ceil(sqrt(n))
+        k = floor(sqrt(k))
+        print int(k - n) + 1
+
+
+
+
+
+
+
+
+'''
 import math
+import itertools
 t=int(raw_input().strip())
 
 for a in range(t):
     n,k = raw_input().strip().split(' ')
     n,k = [int(n),int(k)]
     m=0
-    for i in xrange(n,k+1):
+    #itertools.count(start=0, step=1)
+    for i in itertools.count(start=n, step=1):
+
         if (math.sqrt(i).is_integer()):
             m=m+1
-    print m
+        if i == k:
+            print m
+            break
+
+'''  
--- hackerRank/test.py
+++ hackerRank/test.py
@@ -1,5 +1,23 @@
 import math
+from math import *
+from itertools import *
+import itertools
 
+print floor(sqrt(24))
+
+'''
+#for i in itertools.count(1):
+ #   print i
+
+for i in itertools.count(start=8, step=1):
+    print i
+    if i == 10:
+        break
+
+#itertools.count(start=0, step=1)
+'''
+
+'''
 print 5/2
 print  int ('5' + '5')
 
@@ -23,7 +41,7 @@ for i in range(5,-1,-1):
 
 for k in range(0,3):
     print k 
-
+'''
 '''
     n,k = raw_input().strip().split(' ')
     n,k = [int(n),int(k)]
