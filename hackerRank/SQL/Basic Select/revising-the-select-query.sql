5060a33803a8e61e2b5d313f8be9ca8e0afe9653
vikramlance <vikram.lance@gmail.com>
2016-05-09 19:00:35 -0500

    sql

--- hackerRank/SQL/Basic Select/revising-the-select-query.sql
+++ hackerRank/SQL/Basic Select/revising-the-select-query.sql
@@ -1,2 +1,5 @@
 select * from city where population > 100000 and countrycode ='USA';
 select name from city where population > 120000 and countrycode ='USA';
+select * from city;
+select * from city where id=1661;
+select * from city where COUNTRYCODE ='JPN';
