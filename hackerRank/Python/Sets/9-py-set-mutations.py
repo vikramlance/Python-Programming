ccc4927fe0160cc4255d21e0ab061d414464f911
vikramlance <vikram.lance@gmail.com>
2016-08-01 21:20:50 -0500

    Create 9-py-set-mutations.py

---
+++ hackerRank/Python/Sets/9-py-set-mutations.py
@@ -0,0 +1,14 @@
+a=raw_input()
+
+
+b=set(map(int, raw_input().split()))
+
+
+N=int(raw_input().strip())
+
+for N in range(0,N):
+	c=raw_input().split()
+	d=set(map(int, raw_input().split()))
+	getattr(b, c[0])(d)	
+
+print str(sum(b))
