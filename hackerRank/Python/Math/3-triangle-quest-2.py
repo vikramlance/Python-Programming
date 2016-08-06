5e2050672b1abe28cadc71efed258a9a7c53d948
vikramlance <vikram.lance@gmail.com>
2016-08-06 18:09:17 -0500

    Create 3-triangle-quest-2.py

---
+++ hackerRank/Python/Math/3-triangle-quest-2.py
@@ -0,0 +1,14 @@
+
+for i in range(1,int(raw_input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print (((10**i - 1)//9)**2)
+
+
+
+'''
+for i in range(1,int(raw_input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print ((int(bin((2**i)-1)[2:]))**2)
+'''    
+'''
+for i in range(1,int(raw_input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print ((int(str(1)*i))**2)
+'''
