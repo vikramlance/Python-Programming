20dcd26a45b4e03591f085cfc835752647c23f0e
vikramlance <vikram.lance@gmail.com>
2016-08-06 12:37:27 -0500

    Create 2-find-angle.py

---
+++ hackerRank/Python/Math/2-find-angle.py
@@ -0,0 +1,10 @@
+import math
+a=int(raw_input())
+b=int(raw_input())
+
+
+x= (math.sqrt(math.pow(a,2) + math.pow(b,2)))/2.0
+
+y=math.acos(b/(2*x))
+
+print str(int(round(math.degrees(y),0)))+ u'\N{DEGREE SIGN}'
