cb532dc2f81cb01cdcfafe53adf505052c8b9a00
vikramlance <vikram.lance@gmail.com>
2016-08-07 19:34:47 -0500

    Update 3-itertools-combinations.py

--- hackerRank/Python/Itertools/3-itertools-combinations.py
+++ hackerRank/Python/Itertools/3-itertools-combinations.py
@@ -3,9 +3,20 @@ from itertools import combinations
 a=raw_input().split()
 #b=sorted(list(combinations(a[0],int(a[1]))))
 for i in range(1,int(a[1])+1):
-    b=sorted(list(combinations(a[0],i)))
+    b=list(combinations(a[0],i))
     h=[]
     for j in b:
         h.append(sorted(j))
     for k in sorted(h):
         print ''.join(map(str, k))  
+
+'''
+Ideal answer
+
+from itertools import combinations
+S,k = raw_input().split()
+for j in range(1,int(k)+1):
+    for i in combinations(sorted(S),j):
+        print "".join(i)
+
+'''
