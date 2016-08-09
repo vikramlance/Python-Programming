714bf3f165878da84e8d82d58d1ab18ec8ea549e
vikramlance <vikram.lance@gmail.com>
2016-08-08 20:35:47 -0500

    Create 7-maximize-it.py

---
+++ hackerRank/Python/Itertools/7-maximize-it.py
@@ -0,0 +1,14 @@
+from itertools import product
+
+k,m=raw_input().split()
+
+c=[]
+
+for i in range(int(k)):
+	b= map(int,(raw_input().split()))
+	c.append(b[1:])
+d=[]	
+for i in list(product(*c)):
+	d.append(   (sum(map(lambda x:x*x,i)))%int(m) )
+
+print max(d)
