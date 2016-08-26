7e579689bc2be59c8207bf8c9fc3e6065e8ba413
vikramlance <vikram.lance@gmail.com>
2016-08-26 15:36:24 -0500

    Update 11-py-check-subset.py

--- hackerRank/Python/Sets/11-py-check-subset.py
+++ hackerRank/Python/Sets/11-py-check-subset.py
@@ -1,3 +1,7 @@
+'''
+https://www.hackerrank.com/challenges/py-check-subset
+'''
+
 for i in range(int(input())): #More than 4 lines will result in 0 score. Blank lines won't be counted. 
     a = int(input()); A = set(input().split()) 
     b = int(input()); B = set(input().split())
