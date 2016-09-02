9b77a611c1e5287216abb1d0fc7a72207a694967
vikramlance <vikram.lance@gmail.com>
2016-09-02 16:47:07 -0500

    Create exceptions.py

---
+++ hackerRank/Python/Exceptions/exceptions.py
@@ -0,0 +1,11 @@
+n= int(input())
+
+for i in range(n):
+    a,b=input().split()
+
+    try:
+        print (int(a)//int(b))
+    except ZeroDivisionError as e:
+    	print ("Error Code:",e)
+    except ValueError as er:
+    	print ("Error Code:",er)
