cfdd8e99a54c2cb5951d5574aff4167cad5208f6
vikramlance <vikram.lance@gmail.com>
2016-09-07 00:37:02 -0500

    Update dealing-with-complex-numbers.py

--- hackerRank/Python/Classes/dealing-with-complex-numbers.py
+++ hackerRank/Python/Classes/dealing-with-complex-numbers.py
@@ -1,3 +1,20 @@
 '''
 https://www.hackerrank.com/challenges/class-1-dealing-with-complex-numbers
 '''
+
+import cmath
+a,b=raw_input().split()
+x,y=raw_input().split()
+
+
+c=complex(float(a),float(b))
+d=complex(float(x),float(y))
+
+
+
+print (c+d)
+print (c-d)
+print (c*d)
+print (c/d)
+print (abs(c))
+print (abs(d))
