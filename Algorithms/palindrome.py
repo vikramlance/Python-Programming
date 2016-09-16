5085ec7bf0c691690d1850a3b09308dde01e2a1a
vikramlance <vikram.lance@gmail.com>
2016-09-15 21:29:49 -0500

    Create palindrome.py

---
+++ Algorithms/palindrome.py
@@ -0,0 +1,17 @@
+a=raw_input()
+n=len(a)
+# use stack for checking if given string is palindrome
+
+stack=list(a)
+print stack
+for i in range(n/2):
+    m=i
+    k=stack.pop()
+    if stack[i]!=k:
+    	break
+
+print m
+if (m+1)==n/2:
+    print "Palindrome"
+else:
+    print "not palindrome"   
