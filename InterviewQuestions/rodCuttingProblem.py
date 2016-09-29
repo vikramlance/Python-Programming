7690f456a4e9188819d44d05664a5cf6062943fb
vikramlance <vikram.lance@gmail.com>
2016-09-28 23:32:03 -0500

    Create rodCuttingProblem.py

---
+++ InterviewQuestions/rodCuttingProblem.py
@@ -0,0 +1,18 @@
+'''
+Problem: Find best way to cut a rod of length nn
+Given: rod of length nn
+
+Assume that each length rod has a price pipi
+
+Find best set of cuts to get maximum price
+Each cut is integer length
+Can use any number of cuts, from 0 to n−1n−1
+No cost for a cut
+
+Rod Cutting Prices
+
+Example rod lengths and values:
+length ii	1	2	3	4	5	6	7	8
+price pipi	1	5	8	9	10	17	17	20
+
+'''
