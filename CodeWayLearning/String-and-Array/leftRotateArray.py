f0bec09360e421ac6b0aa4868987f9c7a44b71bf
vikramlance <vikram.lance@gmail.com>
2016-10-14 00:44:41 -0500

    Update leftRotateArray.py

--- CodeWayLearning/String-and-Array/leftRotateArray.py
+++ CodeWayLearning/String-and-Array/leftRotateArray.py
@@ -10,3 +10,28 @@ a[::-1]
 a[:n-k+1:-1]
 a[n-k::-1]
 print a
+
+
+'''
+
+rotating array right by n places
+
+Rotate a one-dimensional array of n elements to the right by k steps. 
+For instance, with n=7 and k=3, the array {a, b, c, d, e, f, g} is rotated to {e, f, g, a, b, c, d}.
+
+def rotate(a,n):
+	l=len(a)
+	c=gcd(l,n)
+	m=l/c
+	step=l-n
+	for i in range(c):
+		 u=a[i]
+		 idx=i
+		 for j in range(1,m):
+			 idx1=(idx+step)%l
+			 a[idx]=a[idx1]
+			 idx=idx1
+		 a[idx]=u
+	print a
+rotate(a,n)
+'''
