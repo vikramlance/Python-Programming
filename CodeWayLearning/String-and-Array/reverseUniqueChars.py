e49e180fedde14597600fdb1937ad872de4a4ed8
vikramlance <vikram.lance@gmail.com>
2016-10-14 00:42:13 -0500

    Create reverseUniqueChars.py

---
+++ CodeWayLearning/String-and-Array/reverseUniqueChars.py
@@ -0,0 +1,6 @@
+'''
+Given a string, print all the unique characters in the string in reverse order. Take input from the prompt screen and write output to a text file.
+	Ex: given: abbccccccdddddddeeeaaaaa
+		output: aedcba
+    
+'''
