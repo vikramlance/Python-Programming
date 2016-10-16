08d585c112e06767494f97fbe60aa61972fbbdc0
vikramlance <vikram.lance@gmail.com>
2016-10-15 23:50:53 -0500

    Update zeroMatrix.py

--- InterviewQuestions/zeroMatrix.py
+++ InterviewQuestions/zeroMatrix.py
@@ -1,8 +1,15 @@
 '''
+Given a boolean matrix, update it so that if any cell is true, all the cells in that row and column are true.
+
+eg. 
+
+[true,  false, false]      [true,  true,  true ]
+[false, false, false]  ->  [true,  false, false]
+[false, false, false]      [true,  false, false]
 
 '''
 
-
+# Assuming True=1, False=0
 a=[[1,0,0],[0,0,0],[0,0,0]]
 
 n= len(a)
