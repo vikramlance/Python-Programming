179141fec489e4b7955b52daf4d1d166d627ec18
vikramlance <vikram.lance@gmail.com>
2016-10-25 21:00:37 -0500

    Update stack.py

--- DataStructures/stack.py
+++ DataStructures/stack.py
@@ -3,18 +3,39 @@ class stack:
     def __init__(self):
 
         self.elements=[]
+        
 
     def isEmpty(self):
-        if self.elements==[]
+        return self.elements==[]
+        	
         
     def size(self):
         return len(self.elements)
 
     def push(self,ob):
         self.elements.append(ob)
+        return self.elements
 
     def pop (self ):
         return self.elements.pop()
 
     def peek (self):
         return self.elements[len(self.elements) - 1]
+        
+st= stack()
+print st.isEmpty()
+
+st.push(1)
+
+print st.isEmpty()
+
+print st.size()
+
+
+st.push(1)
+
+print st.isEmpty()
+
+print st.size()
+
+print st.push(1)
