479ccceb887b9588a795e4b8254fc23f8a82fe45
vikramlance <vikram.lance@gmail.com>
2016-11-06 16:36:33 -0600

    Create counting-mistakes.py

---
+++ NCR-Code-Sprint/counting-mistakes.py
@@ -0,0 +1,25 @@
+'''
+refer pdf doc for problem statement
+
+'''
+
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+starList= map(int, raw_input().split())
+m=len(starList)
+count=0
+oldStar=starList[0]
+for i in range(m):
+    if i==0:
+        if starList[i] !=1 :
+            count+=1
+    else:
+        newStar=starList[i]
+        if newStar !=oldStar + 1:
+            count+=1
+        oldStar=newStar
+print count
