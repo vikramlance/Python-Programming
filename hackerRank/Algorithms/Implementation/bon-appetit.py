0ff14c34baf6a1ad3629f5368fc57bc415943f53
vikramlance <vikram.lance@gmail.com>
2016-11-28 23:57:17 -0600

    Create bon-appetit.py

---
+++ hackerRank/Algorithms/Implementation/bon-appetit.py
@@ -0,0 +1,12 @@
+
+n,k=raw_input().split()
+n=int(n)
+k=int(k)
+
+a=map(int, raw_input().split())
+b=int(raw_input()) 
+
+if (2*b== (sum(a) - a[k])):
+    print "Bon Appetit"
+else:
+    print ( b - ((sum(a) - a[k])//2))
