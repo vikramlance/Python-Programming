f297f3179e2a616378a332df5e9efdd238def3be
vikramlance <vikram.lance@gmail.com>
2016-11-30 23:55:21 -0600

    Update sock-merchant.py

--- hackerRank/Algorithms/Implementation/sock-merchant.py
+++ hackerRank/Algorithms/Implementation/sock-merchant.py
@@ -3,3 +3,12 @@ https://www.hackerrank.com/challenges/sock-merchant
 
 '''
 
+
+import sys
+import itertools
+
+
+n = int(raw_input().strip())
+c = map(int,raw_input().strip().split(' '))
+
+b = c.counter() 
