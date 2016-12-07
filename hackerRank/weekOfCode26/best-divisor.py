4f31e87614cb0d92b451871870b538ddbd10f67c
vikramlance <vikram.lance@gmail.com>
2016-12-06 19:25:01 -0600

    Create best-divisor.py

---
+++ hackerRank/weekOfCode26/best-divisor.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+
+for i in range( 
+
+import math
+
+def divisorGenerator(n):
+    large_divisors = []
+    for i in xrange(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            yield i
+            if i*i != n:
+                large_divisors.append(n / i)
+    for divisor in reversed(large_divisors):
+        yield divisor
+
+k= list(divisorGenerator(100))
