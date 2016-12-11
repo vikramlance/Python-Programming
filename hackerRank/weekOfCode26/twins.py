20077b3a942d330fc4da48e88f4b187ed7f8bf8b
vikramlance <vikram.lance@gmail.com>
2016-12-10 23:59:39 -0600

    Update twins.py

--- hackerRank/weekOfCode26/twins.py
+++ hackerRank/weekOfCode26/twins.py
@@ -1,7 +1,6 @@
 '''
 Problem statement is available in pdf file - Twins _ Week of Code 26 Question _ Contests _ HackerRank.pdf
 '''
-
 import math
 
 n,m = raw_input().split()
@@ -11,9 +10,13 @@ def prime(n):
     for i in range (2,int(n**0.5) + 1):
         if n%i==0:
             return "NO"
+    if n==1:
+        return "NO"
     return "YES"
 count=0
-for i in range(n,m-1):
+if n%2==0:
+    n=n+1
+for i in range(n,m-1,2):
     if prime(i)=='YES' and prime(i+2)=='YES':
         count+=1
 print count
