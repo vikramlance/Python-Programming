41dd4d0f7535280195e22e0be3dec90035a08e25
vikramlance <vikram.lance@gmail.com>
2016-12-12 23:56:48 -0600

    Update twins.py

--- hackerRank/weekOfCode26/twins.py
+++ hackerRank/weekOfCode26/twins.py
@@ -1,17 +1,22 @@
 '''
 Problem statement is available in pdf file - Twins _ Week of Code 26 Question _ Contests _ HackerRank.pdf
 '''
+
 import math
 
 n,m = raw_input().split()
 n=int(n)
 m=int(m)
+primes = []
 def prime(n):
+    if n in primes:
+        return "YES"        
     for i in range (2,int(n**0.5) + 1):
         if n%i==0:
             return "NO"
     if n==1:
         return "NO"
+    primes.append(n)
     return "YES"
 count=0
 if n%2==0:
