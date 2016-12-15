f00a98a20297ea763d09001707cc4769b8c05d38
vikramlance <vikram.lance@gmail.com>
2016-12-14 23:41:54 -0600

    Update street-parade-1.py

--- hackerRank/weekOfCode26/street-parade-1.py
+++ hackerRank/weekOfCode26/street-parade-1.py
@@ -1,3 +1,3 @@
 '''
-Music on the Street _ Week of Code 26 Question _ Contests _ HackerRank.pdf
+Problem statement is available in pdf file - Music on the Street _ Week of Code 26 Question _ Contests _ HackerRank.pdf
 '''
