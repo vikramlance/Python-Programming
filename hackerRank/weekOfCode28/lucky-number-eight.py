12fef653bfc137c7b23d06ffeaf6920a6b401862
vikramlance <vikram.lance@gmail.com>
2017-01-16 00:49:23 -0600

    Update lucky-number-eight.py

--- hackerRank/weekOfCode28/lucky-number-eight.py
+++ hackerRank/weekOfCode28/lucky-number-eight.py
@@ -5,3 +5,29 @@ https://www.hackerrank.com/contests/w28/challenges/lucky-number-eight
 
 '''
 
+#!/bin/python
+
+import sys
+from itertools import combinations
+
+n = int(raw_input().strip())
+number = raw_input().strip()
+# your code goes here
+h=[]
+g=[]
+k = len(number)
+for j in range(1,int(k)+1):
+    b= list(combinations(number,j))
+    for j in b:
+        h.append(j)
+    for k in (h):
+        z = int(''.join(map(str, k)))
+        g.append(z)
+count=0
+
+
+for m in set(g):
+    if m%8==0 :
+        count+=1
+        
+print ((count % ( (10**9) + 7 )) )
