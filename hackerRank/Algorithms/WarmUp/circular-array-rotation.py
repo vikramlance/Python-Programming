5f79001429ba3a1b3619b1b017c4fc405b649e4d
vikramlance <vikram.lance@gmail.com>
2017-01-18 22:35:13 -0600

    Create circular-array-rotation.py

---
+++ hackerRank/Algorithms/WarmUp/circular-array-rotation.py
@@ -0,0 +1,20 @@
+'''
+
+'''
+
+#!/bin/python
+
+import sys
+
+
+n,k,q = raw_input().strip().split(' ')
+n,k,q = [int(n),int(k),int(q)]
+a = map(int,raw_input().strip().split(' '))
+
+new = a[:(n-k)][::-1]
+new1= a[(n-k):][::-1]
+h = new + new1
+final =  h[::-1]
+for a0 in xrange(q):
+    m = int(raw_input().strip())
+    print final[m]
