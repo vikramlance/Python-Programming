af92a80fef58d19d2bee34f06a31e54ce34c3e08
vikramlance <vikram.lance@gmail.com>
2017-03-22 22:58:54 -0500

    Update melodious-password.py

--- hackerRank/WeekOfCode30/melodious-password.py
+++ hackerRank/WeekOfCode30/melodious-password.py
@@ -10,9 +10,12 @@ import itertools
 # from itertools import
 #itertools.permutations(iterable[, r])
 from itertools import permutations
-
+consonant = 'bcdfghjklmnpqrstvwxz'
+vowel ='aeiou' 
 #n = int(raw_input().strip())
 
+for i in consonant:
+  print i
 '''
 perms = [''.join(p) for p in itertools.permutations(iterable['stack', 2])]
 print perms
@@ -22,4 +25,4 @@ from itertools import product
 from string import ascii_lowercase
 keywords = [''.join(i) for i in product(ascii_lowercase,repeat = 2)]
 
-print keywords
+#print keywords
