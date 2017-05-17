896be01e2b150e0d902c48f422439aef208b61de
vikramlance <vikram.lance@gmail.com>
2017-05-16 23:57:54 -0500

    Update hot-and-cold.py

--- hackerRank/HourRank20/hot-and-cold.py
+++ hackerRank/HourRank20/hot-and-cold.py
@@ -3,3 +3,15 @@ https://www.hackerrank.com/contests/hourrank-20/challenges/hot-and-cold
 '''
 
 
+#!/bin/python
+
+import sys
+
+def isSatisfiable(c1, c2, h1, h2):
+    # Complete this function
+
+# Return "YES" if all four conditions can be satisfied, and "NO" otherwise
+c1, c2, h1, h2 = raw_input().strip().split(' ')
+c1, c2, h1, h2 = [int(c1), int(c2), int(h1), int(h2)]
+result = isSatisfiable(c1, c2, h1, h2)
+print(result)
