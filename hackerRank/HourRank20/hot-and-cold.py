17b9df5cac75223b07839f2e41245568ae58f787
vikramlance <vikram.lance@gmail.com>
2017-05-20 23:58:26 -0500

    Update hot-and-cold.py

--- hackerRank/HourRank20/hot-and-cold.py
+++ hackerRank/HourRank20/hot-and-cold.py
@@ -8,6 +8,18 @@ https://www.hackerrank.com/contests/hourrank-20/challenges/hot-and-cold
 import sys
 
 def isSatisfiable(c1, c2, h1, h2):
+    if c1<=c2 :
+        lower = c2
+    else:
+        lower=c1
+    if h1<=h2:
+        higher = h1
+    else:
+        higher= h2
+    if lower<=higher:
+        return "YES"
+    else:
+        return "NO"
     # Complete this function
 
 # Return "YES" if all four conditions can be satisfied, and "NO" otherwise
@@ -15,3 +27,4 @@ c1, c2, h1, h2 = raw_input().strip().split(' ')
 c1, c2, h1, h2 = [int(c1), int(c2), int(h1), int(h2)]
 result = isSatisfiable(c1, c2, h1, h2)
 print(result)
+
