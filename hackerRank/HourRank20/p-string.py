89d74b189c957e1849ecb122654a413a42eaf632
vikramlance <vikram.lance@gmail.com>
2017-05-21 00:02:42 -0500

    Update p-string.py

--- hackerRank/HourRank20/p-string.py
+++ hackerRank/HourRank20/p-string.py
@@ -1,3 +1,19 @@
 '''
 https://www.hackerrank.com/contests/hourrank-20/challenges/p-string
 '''
+
+
+#!/bin/python
+
+import sys
+
+def countSubs(s):
+    # Complete this function
+
+# Return the number of non-empty perfect subsequences mod 1000000007
+q = int(raw_input().strip())
+for a0 in xrange(q):
+    s = raw_input().strip()
+    result = countSubs(s)
+    print(result)
+
