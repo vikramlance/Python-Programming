c618a56566a2b12b513b924915cfc7239faf4597
vikramlance <vikram.lance@gmail.com>
2017-05-26 23:57:17 -0500

    Create arrays-ds.py

---
+++ hackerRank/data-structures/arrays/arrays-ds.py
@@ -0,0 +1,14 @@
+'''
+https://www.hackerrank.com/challenges/arrays-ds
+
+'''
+
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+
+print ' '.join(map(str, arr[::-1]))
