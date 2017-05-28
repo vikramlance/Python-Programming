dd185ef2f767e0aba832f5c3b312d3fce020398f
vikramlance <vikram.lance@gmail.com>
2017-05-27 23:49:28 -0500

    Update 2d-array.py

--- hackerRank/data-structures/arrays/2d-array.py
+++ hackerRank/data-structures/arrays/2d-array.py
@@ -2,3 +2,17 @@
 https://www.hackerrank.com/challenges/2d-array
 
 '''
+
+import sys
+
+
+arr = []
+for arr_i in xrange(6):
+    arr_temp = map(int,raw_input().strip().split(' '))
+    arr.append(arr_temp)
+print arr
+sum =0
+for i in range(4):
+  for j in range (i,i+3):
+    sum= sum + arr[i][j]
+    print sum
