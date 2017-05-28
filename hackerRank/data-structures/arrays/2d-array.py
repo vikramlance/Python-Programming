e4c7bb52f6353756f7d8add540c334bb1e478444
vikramlance <vikram.lance@gmail.com>
2017-05-28 00:42:01 -0500

    Update 2d-array.py

--- hackerRank/data-structures/arrays/2d-array.py
+++ hackerRank/data-structures/arrays/2d-array.py
@@ -3,6 +3,8 @@ https://www.hackerrank.com/challenges/2d-array
 
 '''
 
+#!/bin/python
+
 import sys
 
 
@@ -10,9 +12,29 @@ arr = []
 for arr_i in xrange(6):
     arr_temp = map(int,raw_input().strip().split(' '))
     arr.append(arr_temp)
-print arr
-sum =0
+# print arr
+# counter=0
+sum_new=0
+sum_old=0
 for i in range(4):
-  for j in range (i,i+3):
-    sum= sum + arr[i][j]
-    print sum
+  for j in range (4):
+    sum=0
+    for k in range (i,i+3):
+      for l in range (j,j+3):
+        # print ("k is %d" %(k)) 
+        # print ("l is %d" %(l))
+        # print arr[k][l]
+        if (k == i+1 and l == j ) or (k==i+1 and l==j+2):
+          # print "not sum"
+          pass
+        else:
+          sum=sum+arr[k][l]
+    
+    sum_new= sum
+    if sum_new > sum_old :
+      sum_old = sum_new
+    # print "sum is %d" %(sum)
+    # counter = counter+1
+    
+print sum_old  
+# print counter   
