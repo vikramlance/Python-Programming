33ee6a8e1578e5d6a7f454e1701df2da76a8124a
vikramlance <vikram.lance@gmail.com>
2017-06-11 23:51:03 -0500

    Create hot-and-cold.py

--- hackerRank/HourRank20/hot-and-cold.py
+++ hackerRank/HourRank20/hot-and-cold.py
@@ -2,10 +2,7 @@
 problem statement
 https://www.hackerrank.com/contests/hourrank-20/challenges/hot-and-cold
 '''
-
-
 #!/bin/python
-
 import sys
 
 def isSatisfiable(c1, c2, h1, h2):
@@ -27,4 +24,6 @@ c1, c2, h1, h2 = raw_input().strip().split(' ')
 c1, c2, h1, h2 = [int(c1), int(c2), int(h1), int(h2)]
 result = isSatisfiable(c1, c2, h1, h2)
 print(result)
+
+# end of the program
 
