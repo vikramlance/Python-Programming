9f80650eb68107a1f4c428a85669cd98fb44775d
vikramlance <vikram.lance@gmail.com>
2017-12-17 23:53:32 -0600

    zigzag arrangement

--- GeeksForGeeks/interviewExpereince/yatra/zigzagArray.py
+++ GeeksForGeeks/interviewExpereince/yatra/zigzagArray.py
@@ -11,3 +11,16 @@ array = [9, 6, 3, 4, 10, 12, 5]
 
 array.sort()
 
+new_array = []
+
+array_len = len(array)
+
+print((array_len - 1) / 2)
+print(array[0: (array_len - 1) / 2])
+print(array[((array_len - 1) / 2): (array_len)])
+
+for i in array[0: (array_len - 1) / 2]:
+    for j in array[((array_len - 1) / 2): (array_len)]:
+        pass
+
+
