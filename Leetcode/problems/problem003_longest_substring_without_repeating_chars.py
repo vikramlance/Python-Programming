ee0e92976aca68c4cb42e4e1d96f1b85ffde3778
vikramlance <vikram.lance@gmail.com>
2018-11-17 02:03:37 -0600

    updated problem statement

--- Leetcode/problems/problem003_longest_substring_without_repeating_chars.py
+++ Leetcode/problems/problem003_longest_substring_without_repeating_chars.py
@@ -1,5 +1,16 @@
-"""Module to find out longest substring without repeatition"""
+"""Module to find out longest substring without repeatition
+
+Given a string, find the length of the longest substring without repeating characters.
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+"""
 
 class longestSubstring():
+
     
 
