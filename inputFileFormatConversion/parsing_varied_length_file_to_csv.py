292e61ed95e7f76204f6ffd87ea0c00716d45075
vikramlance <vikram.lance@gmail.com>
2019-01-16 03:33:36 -0600

    Added code to parse varied length file to csv

---
+++ inputFileFormatConversion/parsing_varied_length_file_to_csv.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+# Create dictionary of file names as key and file data as value (list of values)
+# Example file_dict = {file_name1: ['line1', 'line2'], file_name2: ['line3', 'line4']}
+file_dict = defaultdict(list)
+with open('./input.txt') as f:
+    for line in f:
+        if line != '\n':
+            # The file name is first 3 chars in each line that is line[:3]
+            # Add line to respective file name 
+            file_dict[line[:3]].append(line)
+print(file_dict)
+for file in file_dict:
+    # Write data of each file to respective csv file
+    print(file)
+    with open(file + '.csv', 'w') as f:
+        for item in file_dict[file]:
+            f.write("%s" % item)
