3910188aac3953b195fa07adc85165dcfe38fd2d
vikramlance <vikram.lance@gmail.com>
2019-09-08 13:33:10 -0500

    (sock merchange challeng)

---
+++ hackerRank/intervirePrepKit/warmUp/sockMerchant.py
@@ -0,0 +1,54 @@
+""" problem link
+https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+For example, there are n=7 socks with colors ar = [1, 2, 1, 2, 1, 3, 2]. There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+
+Function Description
+
+Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+
+sockMerchant has the following parameter(s):
+
+n: the number of socks in the pile
+ar: the colors of each sock
+
+"""
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    temp = []
+    socks = 0
+    for i in ar:
+        if i in temp:
+            socks = socks + 1
+            temp.remove(i)
+        else:
+            temp.append(i)
+
+    return socks
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
