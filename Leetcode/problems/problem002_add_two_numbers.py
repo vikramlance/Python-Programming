4f9b57e0f5f41011ed524e1fac0f13f654d611b0
vikramlance <vikram.lance@gmail.com>
2019-11-10 00:15:15 -0600

    code cleanup for add two numbers

--- Leetcode/problems/problem002_add_two_numbers.py
+++ Leetcode/problems/problem002_add_two_numbers.py
@@ -20,23 +20,20 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l1_last_digit = l1.val 
+        l1_last_digit = l1.val
         l1_middle_digit = l1.next.val
         l1_first_digit = l1.next.next.val
 
-        l2_last_digit = l2.val 
+        l2_last_digit = l2.val
         l2_middle_digit = l2.next.val
         l2_first_digit = l2.next.next.val
 
         number_sum = (l1_first_digit*100 + l1_middle_digit*10 + l1_last_digit) + (l2_first_digit*100 + l2_middle_digit*10 + l2_last_digit)
 
         number_sum = str(number_sum)
-        
         l3 = ListNode(int(number_sum[2]))
-        l4 = ListNode(int(number_sum[1]))
-        l5 = ListNode(int(number_sum[0]))
-        l3.next = l4
-        l3.next.next = l5
+        l3.next = ListNode(int(number_sum[1]))
+        l3.next.next = ListNode(int(number_sum[0]))
 
         return l3.val, l3.next.val, l3.next.next.val
 
@@ -44,16 +41,12 @@ class Solution:
 out_solution = Solution()
 
 l1 = ListNode(2)
-l1_1 = ListNode(4)
-l1_2 = ListNode(3)
-l1.next = l1_1
-l1.next.next = l1_2
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
 
 l2 = ListNode(5)
-l2_1 = ListNode(6)
-l2_2 = ListNode(4)
-l2.next = l2_1
-l2.next.next = l2_2
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
 
 output = out_solution.addTwoNumbers(l1, l2)
 
