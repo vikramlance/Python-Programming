5c9ee3bd35eb875e8a974920bd45b8c8252797e0
vikramlance <vikram.lance@gmail.com>
2019-11-12 00:08:57 -0600

    changes for median of two sorted arrays

--- Leetcode/problems/Problem004_meadian_two_sorted_arrays_leetcode_sub.py
+++ Leetcode/problems/Problem004_meadian_two_sorted_arrays_leetcode_sub.py
@@ -16,37 +16,44 @@ a = [1,3]
 b = [2] 
 temp_array = []
 
-while a or b:
-    print("abc")
-    print(a, b)
-    if a:
-        a_least_num = a[0]
-    if b:
-        b_least_num = b[0]
-    if not a and b:
-        temp_array.extend(b)
-        break
-    if not b and a:
-        temp_array.extend(a)
-        break
-    if a_least_num < b_least_num:
-        temp_array.append(a_least_num)
-        print(temp_array)
-        del a[0]
-    elif a_least_num > b_least_num:
-        temp_array.append(b_least_num)
-        del b[0]
-    else:
-        temp_array.append(a_least_num)
-        temp_array.append(b_least_num)
-        del a[0]
-        del b[0]
-print("wow")
-print(temp_array)
-total_nums = 0
-total_nums = len(temp_array)
-if total_nums % 2 == 0:     
-    print(total_nums//2)
-    print((temp_array[total_nums // 2] + temp_array[(total_nums // 2) - 1]) / 2)
-else:
-    print(temp_array[total_nums // 2] )
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:        
+        a = nums1
+        b = nums2
+        temp_array = []
+
+        while a or b:
+            if a:
+                a_least_num = a[0]
+            if b:
+                b_least_num = b[0]
+            if not a and b:
+                temp_array.extend(b)
+                break
+            if not b and a:
+                temp_array.extend(a)
+                break
+            if a_least_num < b_least_num:
+                temp_array.append(a_least_num)
+                del a[0]
+            elif a_least_num > b_least_num:
+                temp_array.append(b_least_num)
+                del b[0]
+            else:
+                temp_array.append(a_least_num)
+                temp_array.append(b_least_num)
+                del a[0]
+                del b[0]
+
+        total_nums = 0
+        total_nums = len(temp_array)
+        if total_nums % 2 == 0:
+            return ((temp_array[total_nums // 2] + temp_array[(total_nums // 2) - 1]) / 2)
+        else:
+            return (temp_array[total_nums // 2] )
+
+test_input = [{'nums1':[1, 3], 'nums2':[2]}, {'nums1':[1, 2], 'nums2':[3, 4]}]
+
+Solution_obj = Solution()
+for item in test_input:
+    print(Solution_obj.findMedianSortedArrays(item['nums1'], item['nums2']))
