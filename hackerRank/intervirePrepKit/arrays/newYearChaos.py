838fb7525d3357f825f45d430f4851fa206854f7
vikramlance <vikram.lance@gmail.com>
2020-05-02 21:43:10 -0500

    New Year Chaos problem

---
+++ hackerRank/intervirePrepKit/arrays/newYearChaos.py
@@ -0,0 +1,30 @@
+"""
+Problem link
+https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+
+
+def minimumBribes(q):
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
