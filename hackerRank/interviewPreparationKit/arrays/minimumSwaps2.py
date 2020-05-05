8f59274d7c02baae415385d997c9178574ba4714
vikramlance <vikram.lance@gmail.com>
2020-05-04 23:23:37 -0500

    added minimum swap problem

--- hackerRank/interviewPreparationKit/arrays/minimumSwaps2.py
+++ hackerRank/interviewPreparationKit/arrays/minimumSwaps2.py
@@ -2,18 +2,23 @@
 Problem link
 https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 """
-import math
-import os
-import random
-import re
-import sys
+
 
 # Complete the minimumSwaps function below.
 def minimumSwaps(arr):
-    pass
+    len_arr = len(arr)
+    swap_num = 0
+
+    for i in range(len_arr):
+        if arr[i] != i + 1:
+            arr[arr.index(i + 1)] = arr[i]
+            arr[i] = i + 1
+            swap_num = swap_num + 1
+
+    return swap_num
+
 
 if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
 
@@ -22,7 +27,3 @@ if __name__ == '__main__':
     res = minimumSwaps(arr)
 
     print(res)
-
-    # fptr.write(str(res) + '\n')
-
-    # fptr.close()
