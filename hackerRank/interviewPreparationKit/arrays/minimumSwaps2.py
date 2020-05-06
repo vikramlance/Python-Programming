33e90130e9259f942089218964dafbaf418c5834
vikramlance <vikram.lance@gmail.com>
2020-05-05 23:13:20 -0500

    updated minimum spaws problem

--- hackerRank/interviewPreparationKit/arrays/minimumSwaps2.py
+++ hackerRank/interviewPreparationKit/arrays/minimumSwaps2.py
@@ -3,8 +3,8 @@ Problem link
 https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 """
 
-
-# Complete the minimumSwaps function below.
+"""
+# below solution times out for few test cases but works correctly on other test cases.
 def minimumSwaps(arr):
     len_arr = len(arr)
     swap_num = 0
@@ -13,6 +13,33 @@ def minimumSwaps(arr):
         if arr[i] != i + 1:
             arr[arr.index(i + 1)] = arr[i]
             arr[i] = i + 1
+            swap_num = swap_num + 1
+
+    return swap_num
+
+
+if __name__ == '__main__':
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    print(res)
+
+"""
+
+
+def minimumSwaps(arr):
+    len_arr = len(arr)
+    swap_num = 0
+    temp = 0
+    for i in range(len_arr):
+        while arr[i] != i + 1:
+            temp = arr[arr[i] - 1]
+            arr[arr[i] - 1] = arr[i]
+            arr[i] = temp
             swap_num = swap_num + 1
 
     return swap_num
