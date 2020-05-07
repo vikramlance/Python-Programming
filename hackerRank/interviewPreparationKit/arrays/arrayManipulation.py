307704ede0d93eab78c56d3211ca1f773b4684fb
vikramlance <vikram.lance@gmail.com>
2020-05-07 12:46:26 -0500

    array manimpuation  with temp dictionary

--- hackerRank/interviewPreparationKit/arrays/arrayManipulation.py
+++ hackerRank/interviewPreparationKit/arrays/arrayManipulation.py
@@ -38,3 +38,43 @@ if __name__ == '__main__':
     print(result)
 
 """
+import operator
+
+
+def arrayManipulation(n, queries):
+    len_queries = len(queries)
+    arr = [0] * n
+    temp = {}
+    max_sum = 0
+
+    for i in range(len_queries):
+        a = queries[i][0]
+        b = queries[i][1]
+        k = queries[i][2]
+
+        for j in range(a - 1, b):
+            if j in temp:
+                temp[j] = temp[j] + k
+            else:
+                temp[j] = k
+
+    max_sum = max(temp.items(), key=operator.itemgetter(1))[1]
+    return max_sum
+
+
+if __name__ == '__main__':
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    print(result)
