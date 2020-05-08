462f06f70164d458708aa331f63219c80f879cbc
vikramlance <vikram.lance@gmail.com>
2020-05-07 23:38:23 -0500

    ransom note problem

---
+++ hackerRank/interviewPreparationKit/dictionariesAndHashmaps/hashTablesRansomNote.py
@@ -0,0 +1,22 @@
+"""
+Problem link
+https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+"""
+
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+
+
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
