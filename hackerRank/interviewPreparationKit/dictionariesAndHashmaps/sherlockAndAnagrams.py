b091969f87159d296422cadc15d98a3350bcc093
vikramlance <vikram.lance@gmail.com>
2020-05-12 19:04:47 -0500

    updated anagram problem

--- hackerRank/interviewPreparationKit/dictionariesAndHashmaps/sherlockAndAnagrams.py
+++ hackerRank/interviewPreparationKit/dictionariesAndHashmaps/sherlockAndAnagrams.py
@@ -2,48 +2,31 @@
 Problem link
 https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 
-test 
+test
 2
 ifailuhkqq
 kkkk
 
-output 
+output
 3
 10
-
-i:1
-f:2
-a:3
-i:4
-l:5
-u:6
-h:7
-k:8
-q:9
-q:10
-
-ii 
-iaf : fai
-qq
-
 """
-
-from itertools import combinations
 
 
 def sherlockAndAnagrams(s):
     temp = {}
-
-    for i in range(len(s)):
-        for j in combinations(s, i):
-            if j in temp:
-                temp[j] += 1
-            else:
-                temp[j] = 0
+    len_s = len(s)
     pairs = 0
-    for j in temp:
-        if temp[j] > 1:
-            pairs = pairs + temp[j]
+    for i in range(len_s):
+        len_substr = i + 1
+        for j in range(len_s - i):
+            sorted_substr = ''.join(sorted(s[j: j + len_substr]))
+            if sorted_substr not in temp:
+                temp[sorted_substr] = 0
+            else:
+                temp[sorted_substr] += 1
+                pairs = pairs + temp[sorted_substr]
+
     return pairs
 
 
