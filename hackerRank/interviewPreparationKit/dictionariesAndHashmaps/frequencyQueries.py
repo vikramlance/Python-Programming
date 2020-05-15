37ea208dc8fa7ba1627304241e1f8a78775f2ecb
vikramlance <vikram.lance@gmail.com>
2020-05-14 23:05:37 -0500

    frequency queries problem

---
+++ hackerRank/interviewPreparationKit/dictionariesAndHashmaps/frequencyQueries.py
@@ -0,0 +1,22 @@
+"""
+Problem link 
+https://www.hackerrank.com/challenges/frequency-queries/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+"""
+
+
+def freqQuery(queries):
+    pass
+
+
+if __name__ == '__main__':
+
+    q = int(input().strip())
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    ans = freqQuery(queries)
+
+    print('\n'.join(map(str, ans)))
