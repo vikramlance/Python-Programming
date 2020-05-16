17a8b46b9bd4863fc094840bc6a6446fbd46ed1d
vikramlance <vikram.lance@gmail.com>
2020-05-15 22:52:03 -0500

    updated count triplets

--- hackerRank/interviewPreparationKit/dictionariesAndHashmaps/countTriplets.py
+++ hackerRank/interviewPreparationKit/dictionariesAndHashmaps/countTriplets.py
@@ -1,15 +1,47 @@
 """
 Problem link
 https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+test input
+4 2
+1 2 2 4
+
+output
+2
+
+i 0 1
+
+temp {
+    0: [1, 2, 4]
+    1: [2, 4]
+    2: [2, 4]
+    3: [4]
+}
+
+0: [1, 2]
+1: [2]
+3: [3]
+4: [2]
+5: [4]
 """
 
 
 def countTriplets(arr, r):
     triplets = 0
     len_arr = len(arr)
-    for i in range(len_arr - 2):
-        if i * r in arr[i + 1:len_arr - 1] and i * r * r in arr[i + 2:len_arr] and indexof(i * r) < indexof(i * r * r):
-            triplets += 1
+    temp = {}
+
+    for i in range(len_arr):
+        temp[i] = [arr[i]]
+        for j in temp:
+            if len(temp[j]) == 1 and temp[j][0] * r == arr[i]:
+                temp[i].append(arr[i])
+            if len(temp[j]) == 2 and temp[j][1] * r == arr[i]:
+                temp[i].append(arr[i])
+
+            if len(temp[j]) == 3:
+                triplets += 1
+
     return triplets
 
 
