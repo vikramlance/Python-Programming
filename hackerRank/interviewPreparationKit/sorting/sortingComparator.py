85b495678d3f8f8e3135df127f69b706ff9cf38a
vikramlance <vikram.lance@gmail.com>
2020-05-21 22:53:39 -0500

    comparator problem

---
+++ hackerRank/interviewPreparationKit/sorting/sortingComparator.py
@@ -0,0 +1,18 @@
+"""
+Problem link 
+https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+"""
+
+
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+
+    def __repr__(self):
+
+    def comparator(a, b):
+
+
+n = int(input())
