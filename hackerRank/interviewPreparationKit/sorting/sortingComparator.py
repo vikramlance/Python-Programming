45202cd865910afb630a6e001189b48a0da7cc7a
vikramlance <vikram.lance@gmail.com>
2020-05-22 23:57:44 -0500

    sorting comparator function update

--- hackerRank/interviewPreparationKit/sorting/sortingComparator.py
+++ hackerRank/interviewPreparationKit/sorting/sortingComparator.py
@@ -13,6 +13,17 @@ class Player:
     def __repr__(self):
 
     def comparator(a, b):
+        if a.score > b.score:
+            return -1
+        elif a.score < b.score:
+            return 1
+        elif a.score == b.score:
+            if a.name > b.name:
+                return 1
+            elif a.name < b.name:
+                return -1
+            else:
+                return 0
 
 
 n = int(input())
