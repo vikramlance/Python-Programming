03c6d9b171e4dedae01af156a937d4975a572ba7
vikramlance <vikram.lance@gmail.com>
2020-05-26 23:55:22 -0500

    updated fraudulent activity notification problem

--- hackerRank/interviewPreparationKit/sorting/fraudulentActivityNotifications.py
+++ hackerRank/interviewPreparationKit/sorting/fraudulentActivityNotifications.py
@@ -1,14 +1,39 @@
 """
 Problem link
 https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+test
+
+9 5
+2 3 4 2 3 6 8 4 5
+
+2
 """
 
 
+def median(arr):
+    med = 0
+    l = len(arr)
+    arr.sort()
+    if (l % 2) == 0:
+        med = (arr[int(l / 2)] + arr[int((l + 1) / 2)]) / 2
+    else:
+        med = arr[int((l - 1) / 2)]
+
+    return med
+
+
 def activityNotifications(expenditure, d):
+    count = 0
+    len_e = len(expenditure)
+    for i in range(d, len_e):
+        print(i, expenditure[i - d:i], median(expenditure[i - d:i]))
+        if median(expenditure[i - d:i]) * 2 <= expenditure[i]:
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     nd = input().split()
 
@@ -20,6 +45,4 @@ if __name__ == '__main__':
 
     result = activityNotifications(expenditure, d)
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(str(result) + '\n')
