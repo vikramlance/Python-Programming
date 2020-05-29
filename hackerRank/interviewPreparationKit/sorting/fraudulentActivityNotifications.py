48716485fd62ccacc36ac9794506e662edb9b4a6
vikramlance <vikram.lance@gmail.com>
2020-05-28 23:51:59 -0500

    updated fraudulent activity problem

--- hackerRank/interviewPreparationKit/sorting/fraudulentActivityNotifications.py
+++ hackerRank/interviewPreparationKit/sorting/fraudulentActivityNotifications.py
@@ -8,6 +8,44 @@ test
 2 3 4 2 3 6 8 4 5
 
 2
+
+# below solution times out for few test cases but works correctly on other test cases.
+
+def median(arr):
+    med = 0
+    l = len(arr)
+    arr.sort()
+    if (l % 2) == 0:
+        med = (arr[int(l / 2)] + arr[int((l + 1) / 2)]) / 2
+    else:
+        med = arr[int((l - 1) / 2)]
+
+    return med
+
+
+def activityNotifications(expenditure, d):
+    count = 0
+    len_e = len(expenditure)
+    for i in range(d, len_e):
+        # print(i, expenditure[i - d:i], median(expenditure[i - d:i]))
+        if median(expenditure[i - d:i]) * 2 <= expenditure[i]:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    expenditure = list(map(int, input().rstrip().split()))
+
+    result = activityNotifications(expenditure, d)
+
+    print(str(result) + '\n')
 """
 
 
@@ -27,7 +65,7 @@ def activityNotifications(expenditure, d):
     count = 0
     len_e = len(expenditure)
     for i in range(d, len_e):
-        print(i, expenditure[i - d:i], median(expenditure[i - d:i]))
+        # print(i, expenditure[i - d:i], median(expenditure[i - d:i]))
         if median(expenditure[i - d:i]) * 2 <= expenditure[i]:
             count += 1
     return count
