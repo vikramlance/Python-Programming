95647cb8ac821521680f3d13d8d2f379a2556481
vikramlance <vikram.lance@gmail.com>
2020-05-30 23:27:28 -0500

    updated problem

--- hackerRank/interviewPreparationKit/sorting/fraudulentActivityNotifications.py
+++ hackerRank/interviewPreparationKit/sorting/fraudulentActivityNotifications.py
@@ -48,9 +48,44 @@ if __name__ == '__main__':
     print(str(result) + '\n')
 """
 import bisect 
-def median(s):
-    n = len(s)
-    # s = sorted(lst)
+
+def counting_sort(the_list):
+    
+    # Count the number of times each value appears.
+    # counts[0] stores the number of 0's in the input
+    # counts[4] stores the number of 4's in the input
+    # etc.
+    counts = [0] * (201)
+    for item in the_list:
+        counts[item] += 1
+
+    # Overwrite counts to hold the next index an item with
+    # a given value goes. So, counts[4] will now store the index
+    # where the next 4 goes, not the number of 4's our
+    # list has.
+    num_items_before = 0
+    for i, count in enumerate(counts):
+        counts[i] = num_items_before
+        num_items_before += count
+
+    # Output list to be filled in
+    sorted_list = [None] * len(the_list)
+
+    # Run through the input list
+    for item in the_list:
+        
+        # Place the item in the sorted list
+        sorted_list[ counts[item] ] = item
+
+        # And, make sure the next item we see with the same value
+        # goes after the one we just placed
+        counts[item] += 1
+
+    return sorted_list
+
+def median(lst):
+    n = len(lst)
+    s = counting_sort(lst)
     return (sum(s[n // 2 - 1:n // 2 + 1]) / 2.0, s[n // 2])[n % 2] if n else None
 
 # def median(arr):
@@ -65,22 +100,33 @@ def median(s):
 #     return med
 
 
+# def activityNotifications(expenditure, d):
+#     count = 0
+#     len_e = len(expenditure)
+#     sorted_e = sorted(expenditure[0:d])
+#     for i in range(d, len_e):
+        
+#         if i == d:
+#             sorted_slice = sorted_e
+#             if median(sorted_slice) * 2 <= expenditure[i]:
+#                 count += 1
+#             # print(i, sorted_slice, median(sorted_slice))
+#         else:
+#             temp_list = sorted_slice[1:]
+#             bisect.insort(temp_list, expenditure[i])  
+#             sorted_slice = temp_list
+#             if median(temp_list) * 2 <= expenditure[i]:
+#                 count += 1
+#             # print(i, temp_list, median(temp_list))
+#     return count
+
 def activityNotifications(expenditure, d):
     count = 0
     len_e = len(expenditure)
-    sorted_e = sorted(expenditure[0:d])
     for i in range(d, len_e):
         # print(i, expenditure[i - d:i], median(expenditure[i - d:i]))
-        if i == d:
-            sorted_slice = sorted_e
-            if median(sorted_slice) * 2 <= expenditure[i]:
-                count += 1
-        else:
-            temp_list = sorted_slice[1:]
-            bisect.insort(temp_list, i)  
-            sorted_slice = temp_list
-            if median(temp_list) * 2 <= expenditure[i]:
-                count += 1
+        if median(expenditure[i - d:i]) * 2 <= expenditure[i]:
+            count += 1
     return count
 
 if __name__ == '__main__':
