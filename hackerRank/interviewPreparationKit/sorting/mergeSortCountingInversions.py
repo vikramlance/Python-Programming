290782bc7f3a17b6517b415aa2dc407c70979244
vikramlance <vikram.lance@gmail.com>
2020-06-04 00:11:32 -0500

    updates

--- hackerRank/interviewPreparationKit/sorting/mergeSortCountingInversions.py
+++ hackerRank/interviewPreparationKit/sorting/mergeSortCountingInversions.py
@@ -2,3 +2,22 @@
 Problem link
 https://www.hackerrank.com/challenges/ctci-merge-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
 """
+
+
+def countInversions(arr):
+    for i in range(len(arr)):
+        pass
+
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = countInversions(arr)
+
+        print(str(result) + '\n')
