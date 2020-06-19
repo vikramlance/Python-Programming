915727faa90acb9a613d0fd4994fb92189204af7
vikramlance <vikram.lance@gmail.com>
2020-06-18 22:39:20 -0500

    added valid string string

---
+++ hackerRank/interviewPreparationKit/stringManipulation/sherlockValidString.py
@@ -0,0 +1,21 @@
+"""
+Problem link
+https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+aabbccddeefghi
+NO
+"""
+
+def isValid(s):
+
+    for i in s:
+        pass
+
+
+if __name__ == '__main__':
+
+    s = input()
+
+    result = isValid(s)
+
+    print(result + '\n')
