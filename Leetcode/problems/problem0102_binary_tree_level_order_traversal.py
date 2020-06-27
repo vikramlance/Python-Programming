02a013383b70015e4006a3ef913db84d986dfd21
vikramlance <vikram.lance@gmail.com>
2020-06-27 18:34:40 -0500

    added bfs problem

--- Leetcode/problems/problem0102_binary_tree_level_order_traversal.py
+++ Leetcode/problems/problem0102_binary_tree_level_order_traversal.py
@@ -15,3 +15,43 @@ return its level order traversal as:
   [15,7]
 ]
 """
+
+from collections import deque
+
+
+class solution:
+    def bfs(self, arr):
+        result = []
+
+        temp_queue = deque()
+
+        temp_queue.append(arr[0])
+
+        i = 0
+
+        while temp_queue:
+            current_level_list = []
+            curr_size_temp_queue = len(temp_queue)
+
+            for j in range(curr_size_temp_queue):
+                current_node = temp_queue.popleft()
+                # add children of current node to queue
+                current_level_list.append(current_node)
+
+                if (2 * i + 1) < len(arr) and arr[2 * i + 1]:
+                    temp_queue.append(arr[2 * i + 1])
+
+                if (2 * i + 2) < len(arr) and arr[2 * i + 2]:
+                    temp_queue.append(arr[2 * i + 2])
+
+                i += 1
+
+            result.append(current_level_list)
+
+        return result
+
+
+arr = [3, 9, 20, None, None, 15, 7]
+test = solution()
+
+print(test.bfs(arr))
