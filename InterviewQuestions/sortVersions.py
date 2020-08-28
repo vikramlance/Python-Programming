5518b0caa004aeac54a4fa4b5f3ba2f9ec71433e
vikramlance <vikram.lance@gmail.com>
2020-08-27 23:48:32 -0500

    Update sortVersions.py

--- InterviewQuestions/sortVersions.py
+++ InterviewQuestions/sortVersions.py
@@ -4,7 +4,9 @@ versions = ['1.3.0.9', '0.2.0', '3.1.2', '0.1.6', '5.0.0', '3.3.3.3', '3.3.3.3.3
 
 
 split based on . 
-arrange numbers based on first digit , then second digit and so on .. 
+Convert list of strings to list of numbers
+Sort list of list and then convert back to list of strings 
+arrange numbers based on first digit, then second digit and so on .. 
 
 """
 
