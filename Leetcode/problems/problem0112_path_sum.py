e2fff0138acf37b8718fd56b0fcad249da7a6b5d
vikramlance <vikram.lance@gmail.com>
2020-09-17 22:31:12 -0500

    Update problem0112_path_sum.py

--- Leetcode/problems/problem0112_path_sum.py
+++ Leetcode/problems/problem0112_path_sum.py
@@ -1,15 +1,6 @@
 """
-
 112. Path Sum
-Easy
 
-2075
-
-510
-
-Add to List
-
-Share
 Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 
 Note: A leaf is a node with no children.
