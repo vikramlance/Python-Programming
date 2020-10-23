3242a0e7efaefa0ff97b5027baaca9306fd06e2d
vikramlance <vikram.lance@gmail.com>
2020-10-22 23:46:56 -0500

    updated fizbuzz

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -4,7 +4,7 @@ Using the programming language of your choice:
 Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 '''
 
-for row in range (1,101):
+for row in range(1,101):
     if row%15==0:
         print "FizzBuzz"
     elif row%3==0:
