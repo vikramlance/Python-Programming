2ebe4c60de8ca8334960aa79b8b15fdcf1f2136c
vikramlance <vikram.lance@gmail.com>
2020-11-14 14:06:10 -0600

    Backspace String Compare

---
+++ Leetcode/problems/problem0844.py
@@ -0,0 +1,73 @@
+"""
+844. Backspace String Compare
+
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters.
+Follow up:
+
+Can you solve it in O(N) time and O(1) space?
+
+"""
+
+
+class Solution:
+    def backspaceCompare(self, S, T):    
+        
+        def edited_str(input_str):
+            from collections import deque
+
+            stack = deque()
+
+            for i in input_str:
+
+                if i == '#':
+                    if stack:
+                        stack.pop()
+                    else:
+                        continue
+                else:                    
+                    stack.append(i)
+
+                print(stack)
+            return list(stack)
+                
+        if edited_str(S) == edited_str(T):
+            return True
+        else:
+            return False
+               
+            
+test = Solution()
+
+
+S= "y#fo##f"
+T = "y#f#o##f"
+
+print(test.backspaceCompare(S,T))
