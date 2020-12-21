0cafa2960037713e9f7f7adb5e2bfa69f7e97ca7
vikramlance <vikram.lance@gmail.com>
2020-12-20 19:49:29 -0600

    minor change

--- Leetcode/problems/problem0074.py
+++ Leetcode/problems/problem0074.py
@@ -1,14 +1,6 @@
 """
 74. Search a 2D Matrix
-Medium
 
-2613
-
-184
-
-Add to List
-
-Share
 Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
 Integers in each row are sorted from left to right.
