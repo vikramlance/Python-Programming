45abafd4764e378f95305763b1f3ee550b8fdd55
vikramlance <vikram.lance@gmail.com>
2021-10-12 14:49:19 -0500

    Update read_password_protected_excel.py

--- readPasswordProtectedExcel/read_password_protected_excel.py
+++ readPasswordProtectedExcel/read_password_protected_excel.py
@@ -7,7 +7,7 @@ import os
 
 
 xlApp = win32com.client.Dispatch('Excel.Application')
-# read password of excel file from password.txt (warning: this will pick last line in tex file as a passwd)
+# read password of excel file from password.txt (warning: this will pick last line in text file as a passwd)
 with open('password.txt') as f:
     for line in f:
         passwd = line
